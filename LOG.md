c4ed89b36e794f627be69bc870af6b31fb6149bc
William SC <glassyeyedfish@gmail.com>
2024-02-18 13:07:44 -0500

    Finished basic encoding function.

---
+++ LOG.md
@@ -0,0 +1,8 @@
+# Developer Log
+
+### 2024-02-17
+- Set up project.
+- Began work on encoding function.
+
+### 2024-02-18
+- Finished work on encoding function.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
 use std::env;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
-fn encode(src: &String, dst: &String) {
-    println!("Time to encode!")
+fn encode(src_path: &str, dst_path: &str) -> std::io::Result<()> {
+    let src_file: File = File::open(src_path)?;
+    let mut buf_reader: BufReader<File> = BufReader::new(src_file);
+    let mut contents: String = String::new();
+
+    buf_reader.read_to_string(&mut contents)?;
+
+    let mut dst_file: File = File::create(dst_path)?;
+    let mut dst_bytes: Vec<u8> = Vec::new();
+    let mut dst_buf: String = String::new();
+    let mut reading_first: bool = true;
+
+    for s in contents.chars() {
+        if !s.is_ascii_hexdigit() {
+            continue;
+        }
+
+        if reading_first {
+            dst_buf.push(s);
+            reading_first = false;
+            continue;
+        }
+
+        dst_buf.push(s);
+        reading_first = true;
+
+        let next_byte: u8 = u8::from_str_radix(dst_buf.as_str(), 16).unwrap();
+        dst_bytes.push(next_byte);
+        dst_buf.clear();
+    }
+
+    dst_file.write_all(dst_bytes.as_slice())?;
+
+    Ok(())
 }
 
 fn main() {
@@ -21,7 +55,9 @@ fn main() {
                 println!("Usage: pike e [src] [dst]");
                 return;
             }
-            encode(&args[2], &args[3]);
+            if let Err(e) = encode(&args[2], &args[3]) {
+                panic!("[ERROR]: {:?}", e)
+            }
         }
         _ => {
             println!("That's not a valid command!");
